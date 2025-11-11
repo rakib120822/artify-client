b52b38c0154fdae078eea2e00a4124f9b3766106
rakib120822 <payelrakibulislam@gmail.com>
2025-11-11 12:57:39 +0600

    login and register form background color change

--- src/pages/authPages/Login.jsx
+++ src/pages/authPages/Login.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 
 function Login() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="hero  min-h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-xl">
         <div className="card-body">
           <fieldset className="fieldset">
             <label className="floating-label"><span>Email</span>
--- src/pages/authPages/Register.jsx
+++ src/pages/authPages/Register.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 
 function Register() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="hero  min-h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-xl">
         <div className="card-body">
           <fieldset className="fieldset">
             <label className="floating-label">
