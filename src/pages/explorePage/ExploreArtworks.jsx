8b0f776e7dcb57fe2f195f5d63e7791bd3e44022
rakib120822 <payelrakibulislam@gmail.com>
2025-11-11 11:51:41 +0600

    complete explore page

--- src/pages/explorePage/ExploreArtworks.jsx
+++ src/pages/explorePage/ExploreArtworks.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Card from "../../components/Card";
 
 function ExploreArtworks() {
-  return <div>this explore artworks page</div>;
+  return (
+    <section className="w-11/12 mx-auto my-40">
+      <div className="flex justify-between items-center mb-20">
+        <h1 className="text-4xl font-extrabold ">
+          All <span className="text-red-800">ArtWorks</span>
+        </h1>
+        <div className="join">
+          <input className="input join-item" type="text" placeholder="search" />
+          <button className="btn join-item  bg-red-800 text-white font-bold">Subscribe</button>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5  ">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+    </section>
+  );
 }
 
 export default ExploreArtworks;
