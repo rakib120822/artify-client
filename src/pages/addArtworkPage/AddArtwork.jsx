9db52b62615b3a584cc8f92b0db56e7c43425b35
rakib120822 <payelrakibulislam@gmail.com>
2025-11-11 12:18:58 +0600

    complete add artwork form

--- src/pages/addArtworkPage/AddArtwork.jsx
+++ src/pages/addArtworkPage/AddArtwork.jsx
@@ -1,7 +1,68 @@
 import React from "react";
 
 function AddArtwork() {
-  return <div>this is add artwork page</div>;
+  return (
+    <div>
+      <div className="hero  min-h-screen">
+        <div className="card bg-base-100 w-full max-w-md shrink-0 shadow-xl">
+          <div className="card-body ">
+            <fieldset className="fieldset">
+              <label className="label">Title</label>
+              <input
+                type="text"
+                required
+                className="input w-full"
+                placeholder="title"
+              />
+              <label className="label">Photo URL</label>
+              <input
+                type="text"
+                required
+                className="input w-full"
+                placeholder="photo URL"
+              />
+              <label className="label">Description</label>
+              <textarea
+                required
+                className="textarea h-24 w-full"
+                placeholder="description"
+              ></textarea>
+              <div className="flex gap-2">
+                <div>
+                  <label className="label">Category</label>
+                  <input
+                    type="text"
+                    className="input w-full"
+                    placeholder="Category"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="label">Medium</label>
+                  <input
+                    type="text"
+                    className="input w-full"
+                    placeholder="photo URL"
+                    required
+                  />
+                </div>
+              </div>
+              <label className="label">Price</label>
+              <input
+                required
+                type="number"
+                className="input w-full"
+                placeholder="Price"
+              />
+              <button className="btn bg-red-800 text-white mt-4">
+                Add My ArtWork
+              </button>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AddArtwork;
--- src/pages/gallaryPage/Gallery.jsx
+++ src/pages/gallaryPage/Gallery.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import GalleryCard from "../../components/GalleryCard";
+
 
 function Gallery() {
   return (
-    <div>
-      this is Gallery page
-    </div>
-  )
+    <section className="w-11/12 mx-auto my-10">
+    
+        <h1 className="text-4xl font-extrabold text-center mb-10">
+          My  <span className="text-red-800">Gallery</span>
+        </h1>
+       
+  
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5  ">
+        
+       <GalleryCard/>
+       <GalleryCard/>
+       <GalleryCard/>
+       <GalleryCard/>
+      </div>
+    </section>
+  );
 }
 
-export default Gallery
+export default Gallery;
